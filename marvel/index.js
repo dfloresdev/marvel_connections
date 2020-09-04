fad28c052b2566ac40192caf9a4de53911519527
dfloresdev <hello@dflores.dev>
2020-09-03 23:09:28 -0500

    [DOCUMENTATION] swagger added

--- marvel/index.js
+++ marvel/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const config = require("../config");
 const app = express();
+const swaggerUI = require("swagger-ui-express");
+const cors = require("cors");
+app.use(cors());
 
 const collaborators = require("./components/collaborators/network");
 const characters = require("./components/characters/network");
 const errors = require("../network/errors");
+const swaggerDoc = require("./swagger.json");
+
 // ROUTER
 app.use("/marvel/collaborators", collaborators);
 app.use("/marvel/characters", characters);
+
+app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use(errors);
 
